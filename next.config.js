910c575b721db3053cfd227196d970a0627c444b
santiagopena171 <santijoelpena@gmail.com>
2026-03-01 00:45:23 -0300

    config: set Telegram bot token and chat ID

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   env: {
     CALLMEBOT_PHONE: process.env.CALLMEBOT_PHONE || '59897784716',
     CALLMEBOT_APIKEY: process.env.CALLMEBOT_APIKEY || '3239720',
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
-    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID || '',
+    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '8681789703:AAF9wm9VA5XOZOskrLocSlkwlh8N_DgOlfM',
+    TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID || '5695426761',
   },
   images: {
     remotePatterns: [
